ac40f89f7f199e866542d73b3d9f5a901681458d
tan90 <43111645+JingzeWang@users.noreply.github.com>
2021-01-09 20:29:59 +0800

    rdt4.0-SR

--- src/com/ouc/tcp/test/TCP_Receiver.java
+++ src/com/ouc/tcp/test/TCP_Receiver.java
@@ -4,15 +4,16 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import com.ouc.tcp.client.TCP_Receiver_ADT;
 import com.ouc.tcp.message.*;
-import com.ouc.tcp.tool.TCP_TOOL;
 
 public class TCP_Receiver extends TCP_Receiver_ADT {
 
 	private TCP_PACKET ackPack; // 回复的ACK报文段
-	int lastSequence = -1;// 用于记录当前待接收的包序号，注意包序号不完全是
+	private ReceiverSlidingWindow window = new ReceiverSlidingWindow();
 
 	/* 构造函数 */
 	public TCP_Receiver() {
@@ -25,24 +26,17 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	public void rdt_recv(TCP_PACKET recvPack) {
 		// 检查校验码，生成ACK
 		if (CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
-			// 生成ACK报文段（设置确认号）
-			tcpH.setTh_ack(recvPack.getTcpH().getTh_seq());
-			ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
-			tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
-			System.out.println();
-			System.out.println("ACK: " + recvPack.getTcpH().getTh_seq());
-			System.out.println();
-			// 回复ACK报文段
-			reply(ackPack);
-
-			// 将接收到的正确有序的数据插入data队列，准备交付
-			int currentSequence = (recvPack.getTcpH().getTh_seq() - 1) / 100;
-			if (currentSequence != lastSequence) {
-				lastSequence = currentSequence;
-				dataQueue.add(recvPack.getTcpS().getData());
-				// 交付数据（每20组数据交付一次）
-				if (dataQueue.size() == 20)
-					deliver_data();
+			int ACKSequence = -1;
+			try {
+				ACKSequence = window.recv_pkt(recvPack.clone());
+			} catch (CloneNotSupportedException e) {
+				e.printStackTrace();
+			}
+			if (ACKSequence != -1) {
+				tcpH.setTh_ack(ACKSequence * 100 + 1);
+				ackPack = new TCP_PACKET(tcpH, tcpS, recvPack.getSourceAddr());
+				tcpH.setTh_sum(CheckSum.computeChkSum(ackPack));
+				reply(ackPack);
 			}
 		}
 		System.out.println();
@@ -52,14 +46,64 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 	@Override
 	// 交付数据（将数据写入文件）；不需要修改
 	public void deliver_data() {
-		// 检查dataQueue，将数据写入文件
-		File fw = new File("recvData.txt");
-		BufferedWriter writer;
+	}
 
+	@Override
+	// 回复ACK报文段
+	public void reply(TCP_PACKET replyPack) {
+		// 设置错误控制标志
+		tcpH.setTh_eflag((byte) 7); // eFlag=0，信道无错误
+
+		// 发送数据报
+		client.send(replyPack);
+	}
+
+}
+
+class ReceiverSlidingWindow {
+	private int size = 16;
+	private int base = 0;
+	private TCP_PACKET[] packets = new TCP_PACKET[size];
+	Queue<int[]> dataQueue = new LinkedBlockingQueue();
+
+	public ReceiverSlidingWindow() { }
+
+	public int recv_pkt(TCP_PACKET packet) {
+		int currentSequence = (packet.getTcpH().getTh_seq() - 1) / 100;
+		if (currentSequence < base) {
+			int l = base - size, r = base - 1;
+			l = Math.max(l, 1);
+			if (l <= currentSequence && currentSequence <= r)
+				return currentSequence;
+		} else if (base <= currentSequence && currentSequence < base + size) {
+			packets[currentSequence - base] = packet;
+			if (currentSequence == base)
+				slid_window();
+			return currentSequence;
+		}
+		return -1;
+	}
+
+	private void slid_window() {
+		int maxIndex = 0;
+		while (maxIndex + 1 < size && packets[maxIndex + 1] != null)
+			maxIndex++;
+		for (int i = 0; i < maxIndex + 1; i++)
+			dataQueue.add(packets[i].getTcpS().getData());
+		for (int i = 0; maxIndex + 1 + i < this.size; i++)
+			packets[i] = packets[maxIndex + 1 + i];
+		for (int i = size - maxIndex - 1; i < size; i++)
+			packets[i] = null;
+		base += maxIndex + 1;
+		if (dataQueue.size() >= 20 || base == 1000)
+			deliver_data();
+	}
+
+	public void deliver_data() {
 		try {
-			writer = new BufferedWriter(new FileWriter(fw, true));
+			File file = new File("recvData.txt");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
 
-			// 循环检查data队列中是否有新交付数据
 			while (!dataQueue.isEmpty()) {
 				int[] data = dataQueue.poll();
 
@@ -67,7 +111,6 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 				for (int i = 0; i < data.length; i++) {
 					writer.write(data[i] + "\n");
 				}
-
 				writer.flush(); // 清空输出缓存
 			}
 			writer.close();
@@ -75,15 +118,4 @@ public class TCP_Receiver extends TCP_Receiver_ADT {
 			e.printStackTrace();
 		}
 	}
-
-	@Override
-	// 回复ACK报文段
-	public void reply(TCP_PACKET replyPack) {
-		// 设置错误控制标志
-		tcpH.setTh_eflag((byte) 4); // eFlag=0，信道无错误
-
-		// 发送数据报
-		client.send(replyPack);
-	}
-
 }
--- src/com/ouc/tcp/test/TCP_Sender.java
+++ src/com/ouc/tcp/test/TCP_Sender.java
@@ -1,18 +1,17 @@
 package com.ouc.tcp.test;
 
+import com.ouc.tcp.client.Client;
 import com.ouc.tcp.client.TCP_Sender_ADT;
 import com.ouc.tcp.client.UDT_RetransTask;
 import com.ouc.tcp.client.UDT_Timer;
 import com.ouc.tcp.message.*;
-import com.ouc.tcp.tool.TCP_TOOL;
 
 public class TCP_Sender extends TCP_Sender_ADT {
 
 	private TCP_PACKET tcpPack; // 待发送的TCP数据报
-	private volatile int flag = 0;
+	private volatile int flag = 1;
 
-	private UDT_Timer timer;
-	private UDT_RetransTask task;
+	private SenderSlidingWindow window = new SenderSlidingWindow(client);
 
 	/* 构造函数 */
 	public TCP_Sender() {
@@ -31,26 +30,28 @@ public class TCP_Sender extends TCP_Sender_ADT {
 
 		tcpH.setTh_sum(CheckSum.computeChkSum(tcpPack));
 		tcpPack.setTcpH(tcpH);
-
-		timer = new UDT_Timer();
-		task = new UDT_RetransTask(client, tcpPack);
-		timer.schedule(task, 500, 500);
-
-		// 发送TCP数据报
-		udt_send(tcpPack);
-		flag = 0;
-
-		// 等待ACK报文
-		// waitACK();
+		if (window.isFull()) {
+			System.out.println();
+			System.out.println("Waiting for Sliding Window...");
+			System.out.println();
+			flag = 0;
+		}
 		while (flag == 0)
 			;
+		try {
+			window.insert_pkt(tcpPack.clone());
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		// 发送TCP数据报
+		udt_send(tcpPack);
 	}
 
 	@Override
 	// 不可靠发送：将打包好的TCP数据报通过不可靠传输信道发送；仅需修改错误标志
 	public void udt_send(TCP_PACKET stcpPack) {
 		// 设置错误控制标志
-		tcpH.setTh_eflag((byte) 4);
+		tcpH.setTh_eflag((byte) 7);
 		// System.out.println("to send: "+stcpPack.getTcpH().getTh_seq());
 		// 发送数据报
 		client.send(stcpPack);
@@ -59,20 +60,6 @@ public class TCP_Sender extends TCP_Sender_ADT {
 	@Override
 	// 需要修改
 	public void waitACK() {
-		// 循环检查ackQueue
-		// 循环检查确认号对列中是否有新收到的ACK
-		if (!ackQueue.isEmpty()) {
-			int currentAck = ackQueue.poll();
-			// System.out.println("CurrentAck: "+currentAck);
-			if (currentAck == tcpPack.getTcpH().getTh_seq()) {
-				System.out.println();
-				System.out.println("Clear: " + tcpPack.getTcpH().getTh_seq());
-				System.out.println();
-				timer.cancel();
-				flag = 1;
-				// break;
-			}
-		}
 	}
 
 	@Override
@@ -81,16 +68,63 @@ public class TCP_Sender extends TCP_Sender_ADT {
 		System.out.println();
 		if (CheckSum.computeChkSum(recvPack) == recvPack.getTcpH().getTh_sum()) {
 			System.out.println("Receive ACK Number： " + recvPack.getTcpH().getTh_ack());
-			ackQueue.add(recvPack.getTcpH().getTh_ack());
-		} else {
-			System.out.println("Receive corrupt ACK: " + recvPack.getTcpH().getTh_ack());
-			ackQueue.add(-1);
+			window.recv_ACK((recvPack.getTcpH().getTh_ack() - 1) / 100);
+			if (!window.isFull())
+				flag = 1;
 		}
-		System.out.println();
-
 		// 处理ACK报文
 		waitACK();
-
 	}
 
+}
+
+class SenderSlidingWindow {
+	private Client client;
+	private int size = 16;
+	private int base = 0;
+	private int nextIndex = 0;
+	private TCP_PACKET[] packets = new TCP_PACKET[size];
+	private UDT_Timer[] timers = new UDT_Timer[size];
+
+	public SenderSlidingWindow(Client cli) {
+		client = cli;
+	}
+
+	public boolean isFull() {
+		return size <= nextIndex;
+	}
+
+	public void insert_pkt(TCP_PACKET packet) {
+		packets[nextIndex] = packet;
+		timers[nextIndex] = new UDT_Timer();
+		timers[nextIndex].schedule(new UDT_RetransTask(client, packet), 500, 500);
+		nextIndex++;
+	}
+
+	public void recv_ACK(int currentSequence) {
+		if (base <= currentSequence && currentSequence < base + size) {
+			if (timers[currentSequence - base] == null)
+				return;
+			timers[currentSequence - base].cancel();
+			timers[currentSequence - base] = null;
+		}
+		if (currentSequence == base) {
+			int maxACKedIndex = 0;
+			while (maxACKedIndex + 1 < nextIndex && timers[maxACKedIndex + 1] == null)
+				maxACKedIndex++;
+
+			for (int i = 0; maxACKedIndex + 1 + i < size; i++) {
+				packets[i] = packets[maxACKedIndex + 1 + i];
+				timers[i] = timers[maxACKedIndex + 1 + i];
+			}
+
+			for (int i = size - (maxACKedIndex + 1); i < size; i++) {
+				packets[i] = null;
+				timers[i] = null;
+			}
+
+			base += maxACKedIndex + 1;
+			nextIndex -= maxACKedIndex + 1;
+		}
+	}
 }
